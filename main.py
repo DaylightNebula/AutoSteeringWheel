e8b20924cca72c280d71b0160d2112dd0dbb8769
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2022-09-10 17:25:03 -0600

    First attempt at asphalt detection

--- main.py
+++ main.py
@@ -8,21 +8,29 @@ import imutils
 import time
 import os
 
+track_high = (200, 200, 200)
+track_low = (120, 120, 120)
+
 
 def processFrame(frame, show):
-    displayFrames([img], show)
+    mask = cv2.inRange(frame, track_low, track_high)
+    mask = cv2.erode(mask, None, iterations=5)
+    mask = cv2.dilate(mask, None, iterations=2)
+    displayFrames([frame, mask], show)
 
 
 def displayFrames(frames, show):
     if show:
+        counter = 0
         for frame in frames:
+            counter += 1
             resizedFrame = cv2.resize(frame, (1280, 720))
-            cv2.imshow("image", resizedFrame)
+            cv2.imshow("image_" + str(counter), resizedFrame)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 
-# test code
+# test code, loop through each image in assets\images and process it
 for filename in os.listdir("assets\\images"):
     f = os.path.join("assets\\images", filename)
     # checking if it is a file
